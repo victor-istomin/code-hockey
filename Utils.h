e9fb9ae67e8572fb37f4f8a3dda7259f0b3b24b1
Victor Istomin <victor.send.mail.here@gmail.com>
2014-09-28 09:55:37 +0300

    release compilation fix

--- Utils.h
+++ Utils.h
@@ -2,10 +2,10 @@
 #define PI 3.14159265358979323846
 #include <algorithm>
 #include <cmath>
+#include <string>
 
 #ifdef _DEBUG
 #	include <windows.h>
-#	include <string>
     inline void debugPrint(const std::string& s)		            {::OutputDebugStringA((s+"\n").c_str());}
     template <typename T> std::string toString(const T& var)        {return std::to_string(var);}
 
